d4cd48cae295b1cc5899573a79f0ad980dac3599
Akash Meshram <6388495akash@gmail.com>
2021-11-13 10:50:40 +0530

    🚑️ upload hotfix

--- src/Board.js
+++ src/Board.js
@@ -1,26 +1,46 @@
-import {EventEmitter} from 'events';
-import { 
+import { EventEmitter } from "events";
+import {
   stylesList,
   contentsList,
-  contentImageId, 
-  styleStrengthId, 
-  styleImageId, 
+  contentImageId,
+  styleStrengthId,
+  styleImageId,
   styleGalleryId,
   contentGalleryId,
-  statusId 
-} from './config';
+  statusId,
+  uploadContentId,
+  uploadStyleId,
+  canvasId,
+} from "./config";
 
-const styleImage = (name, folder) => {
-  const img = document.createElement('img');
+const addImage = (name, folder) => {
+  const img = document.createElement("img");
   img.src = `./img/${folder}/${name}.webp`;
   img.alt = `${name} style image`;
   img.id = `style-${name}`;
   return img;
-} 
+};
+
+function uploadFile(button, target) {
+  button.addEventListener("input", function () {
+    if (this.files && this.files[0]) {
+      const reader = new FileReader();
+      reader.onload = function (e) {
+        target.setAttribute("src", e.target.result);
+      };
+      reader.readAsDataURL(this.files[0]);
+    }
+  });
+}
 
 class Board extends EventEmitter {
   constructor() {
     super();
+
+    this.uploadContent = document.getElementById(uploadContentId);
+    this.uploadStyle = document.getElementById(uploadStyleId);
+    this.canvas = document.getElementById(canvasId);
+
     this.styleGallery = document.getElementById(styleGalleryId);
     this.contentGallery = document.getElementById(contentGalleryId);
 
@@ -28,57 +48,86 @@ class Board extends EventEmitter {
     this.strengthBar = document.getElementById(styleStrengthId);
     this.content = document.getElementById(contentImageId);
     this.status = document.getElementById(statusId);
-
     this.statusMessages = [
-      {color: 'black', message: 'Click transfer to apply strength'},
-      {color: '#f44336ff', message: 'Style Processing ....'},
-      {color: 'red', message: 'Style Applying ....'},
-      {color: 'green', message: 'Stylize Complete !!!'}
+      { color: "black", message: "Click transfer to apply strength" },
+      { color: "#f44336ff", message: "Style Processing ...." },
+      { color: "red", message: "Style Applying ...." },
+      { color: "green", message: "Stylize Complete !!!" },
     ];
 
+    this.loadStrengthBar();
+    this.loadUploadContent();
+    this.loadUploadStyle();
     this.loadStyles();
     this.loadContent();
-    this.loadStrengthBar();
+    this.setUpCanvas();
   }
 
   loadStrengthBar() {
-    this.strengthBar.addEventListener('input', (e) => {
-      this.emit('strength',  e.target.value/100.);
-    })
+    this.strengthBar.addEventListener("input", (e) => {
+      this.emit("strength", e.target.value / 100);
+    });
+  }
+
+  loadUploadContent() {
+    uploadFile(this.uploadContent, this.content);
+  }
+
+  loadUploadStyle() {
+    uploadFile(this.uploadStyle, this.style);
   }
 
   loadStyles() {
-    stylesList.sort(() => 0.5 - Math.random()).map(name => {
-      const image = styleImage(name, 'styles');
-      const cell = document.createElement('button');
-      cell.appendChild(image);
-      cell.addEventListener('click', () => { this.style.src =  image.src;});
-      this.styleGallery.appendChild(cell);
-    })    
+    stylesList
+      .sort(() => 0.5 - Math.random())
+      .map((name) => {
+        const image = addImage(name, "styles");
+        const cell = document.createElement("button");
+        cell.appendChild(image);
+        cell.addEventListener("click", () => {
+          this.style.src = image.src;
+        });
+        this.styleGallery.appendChild(cell);
+      });
   }
 
   loadContent() {
-    contentsList.sort(() => 0.5 - Math.random()).map(name => {
-      const image = styleImage(name, 'contents');
-      const cell = document.createElement('button');
-      cell.appendChild(image);
-      cell.addEventListener('click', () => { this.content.src =  image.src;});
-      this.contentGallery.appendChild(cell);
-    })    
+    contentsList
+      .sort(() => 0.5 - Math.random())
+      .map((name) => {
+        const image = addImage(name, "contents");
+        const cell = document.createElement("button");
+        cell.appendChild(image);
+        cell.addEventListener("click", () => {
+          this.content.src = image.src;
+        });
+        this.contentGallery.appendChild(cell);
+      });
+  }
+
+  setUpCanvas() {
+    this.canvas.addEventListener("click", () => {
+      const image = this.canvas
+        .toDataURL("image/png")
+        .replace("image/png", "image/octet-stream");
+      const link = document.createElement("a");
+      link.download = "styled.png";
+      link.href = image;
+      link.click();
+    });
   }
 
   set contentImage(src) {
-    this.content.setAttribute('src', src);
+    this.content.setAttribute("src", src);
   }
 
   set styleImage(src) {
-    this.style.setAttribute('src', src);
+    this.style.setAttribute("src", src);
   }
 
   set statusCode(value) {
-    this.status.innerHTML = this.statusMessages[value]['message'];
+    this.status.innerHTML = this.statusMessages[value]["message"];
   }
-
 }
 
 export default Board;
--- src/index.js
+++ src/index.js
@@ -1,47 +1,16 @@
 import Machine from './Machine';
 import Board from './Board';
-import { transfer, uploadContentId, uploadStyleId, canvasId } from './config';
+import { transfer, canvasId } from './config';
 
 const tranferButton = document.getElementById(transfer);
-const uploadContent = document.getElementById(uploadContentId);
-const uploadStyle = document.getElementById(uploadStyleId);
-const canvas = document.getElementById(canvasId);
 
 const board = new Board();
 const machine  = new Machine();
 machine.loadModels();
 
-uploadContent.addEventListener('input', function() {
-  if (this.files && this.files[0]) {
-    const reader = new FileReader();
-    reader.onload = function (e) {
-      board.contentImage =  e.target.result;
-    };
-    reader.readAsDataURL(this.files[0]);
-  }
-});
-
-uploadStyle.addEventListener('input', function() {
-  if (this.files && this.files[0]) {
-    const reader = new FileReader();
-    reader.onload = function (e) {
-      board.styleImage =  e.target.result;
-    };
-    reader.readAsDataURL(this.files[0]);
-  }
-});
-
 tranferButton.addEventListener('click', () => {
   machine.applyStyle();
-})  
-
-canvas.addEventListener('click', () => {
-  const image = canvas.toDataURL("image/png", 1.0).replace("image/png", "image/octet-stream");
-  const link = document.createElement('a');
-  link.download = "styled.png";
-  link.href = image;
-  link.click();
-})  
+}) 
 
 board.on('strength', (val) => {
   machine.styleStrength = val;
